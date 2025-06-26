99e96d5a2d2e22233d685ea6ed45761b875cf8dd
Muhammad Ali <168365754+muhammadali-ip91@users.noreply.github.com>
2025-06-26 13:01:42 +0500

    initial commit

---
+++ day 3/split_join.py
@@ -0,0 +1,13 @@
+
+sentence = input('write your sentence : ')
+
+split_sentence = sentence.split()
+print('word list is :' , split_sentence )
+
+join_sentence = "-".join(split_sentence)
+print('join sentence :' , join_sentence)
+
+name_tuple = ("Muhammad", "Ali")
+
+print("First Name:", name_tuple[0])
+print("Last Name:", name_tuple[1])
